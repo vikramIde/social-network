ccfe230cb0a0a76d26875cae29bf5243e649d0ca
vikramide <>
2021-07-25 01:26:24 +0530

    Harmony Updates

---
+++ README.md
@@ -0,0 +1,7 @@
+Steps
+
+npm install
+truffle compile
+truffle migrate --reset --network testnet
+npm start which should popup http://localhost:3000/ page
+make sure to connect metamask to harmony testnet, as the social-network contracts are deployed on harmony testnet
--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,10 @@
 require('babel-register');
 require('babel-polyfill');
-
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const privateKeys = process.env.PRIVATE_KEYS || ""
+const testnet = process.env.TESTNET;
+const mainnet = process.env.MAINNET;
 module.exports = {
   networks: {
     development: {
@@ -8,6 +12,16 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    testnet: {
+      provider: () => new HDWalletProvider(privateKeys.split(','), testnet),
+      network_id: 1666700000,
+      skipDryRun: true,
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(privateKeys.split(','), mainnet),
+      network_id: 1666600000,
+      skipDryRun: true,
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
